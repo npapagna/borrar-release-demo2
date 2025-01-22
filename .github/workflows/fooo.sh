9a33ead7b22599cdbb4f874e5f38ef0682c755cc
nicopm <nicolas.papagna@pan-energy.com>
2025-01-22 15:15:21 -0300

    test

--- .github/workflows/fooo.sh
+++ .github/workflows/fooo.sh
@@ -2,23 +2,22 @@
 
 set -euo
 
-REPOSITORIES=(
-"https://github.com/Garuflax/cuis-testlint.git"
-"https://github.com/hernanwilkinson/Cuis-Smalltalk-DenotativeObject.git"
-"https://github.com/hernanwilkinson/Cuis-Smalltalk-Refactoring.git"
-"https://github.com/hernanwilkinson/Cuis-University.git"
-"https://github.com/hernanwilkinson/LiveTyping.git"
-"https://github.com/hernanwilkinson/SystemCategoryAutoSaving.git"
-"https://github.com/hernanwilkinson/TDDGuru.git"
-"https://github.com/hernanwilkinson/cuis-finder-asWidget.git"
-"https://github.com/maxi1985798/cuis-extract-class-refactoring.git"
-"https://github.com/maxi1985798/cuis-move-instance-variable-refactoring.git"
-"https://github.com/maxi1985798/move-method-refactoring.git"
-)
+clone_repository() {
+  git clone --single-branch --depth 1 "$1"
+}
 
 rm -rf work
 mkdir work
 cd work
-for repositoryURL in ${REPOSITORIES[@]}; do
-  git clone --single-branch --depth 1 $repositoryURL
-done
+
+clone_repository "https://github.com/Garuflax/cuis-testlint.git"
+clone_repository "https://github.com/hernanwilkinson/Cuis-Smalltalk-DenotativeObject.git"
+clone_repository "https://github.com/hernanwilkinson/Cuis-Smalltalk-Refactoring.git"
+clone_repository "https://github.com/hernanwilkinson/Cuis-University.git"
+clone_repository "https://github.com/hernanwilkinson/LiveTyping.git"
+clone_repository "https://github.com/hernanwilkinson/SystemCategoryAutoSaving.git"
+clone_repository "https://github.com/hernanwilkinson/TDDGuru.git"
+clone_repository "https://github.com/hernanwilkinson/cuis-finder-asWidget.git"
+clone_repository "https://github.com/maxi1985798/cuis-extract-class-refactoring.git"
+clone_repository "https://github.com/maxi1985798/cuis-move-instance-variable-refactoring.git"
+clone_repository "https://github.com/maxi1985798/move-method-refactoring.git"
